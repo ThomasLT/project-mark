07b09f5768a3c97940fbd7552b04f79228a7b571
ThomasLT <1901302@LR-D121-080M.staff.southend.ac.uk>
2015-10-08 16:23:12 +0100

    Updated scripts and added lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -30,36 +30,40 @@ $('#version').html("Version 0.13")
 		$(this).parent().children(".tab").focus();
 	}
 });
-var q = function() { $(".menu .active").removeClass("active"); }; 		
-$('.lvl1, .tab, .lvl2, #cover, #content, #closecontent').clickoutside(q);
-
+// Set function for click outside
+var RemoveActive = function() { $(".menu .active").removeClass("active"); };
+// When click outside remove ative class
+$('.lvl1, .tab, .lvl2, #cover, #content, #closecontent').clickoutside(RemoveActive);
+// This is what loads the content
 $('.lvl2 a').on('click', function(){
-	var frame = "/project-mark/"+$(this).attr('data-frame');
-	
+    // Collect url from data-frame attribute
+	var frame = $(this).attr('data-frame');
+	// On load - load html from url - display error code if needed
 	$('#content').load(frame, function( response, status, xhr ) {
 		if ( status == "error" ) {
 			var msg = "Sorry something went wrong ";
 			$( "#content" ).html( msg + xhr.status + " " + xhr.statusText );
 		}
 	});
-	
+    // Add active class
 	$('#frame').addClass("active");
 	$("#cover").addClass("active");
 });
+// Remove active close on click close or outside the box, also resets the content to say loading
 $('#closecontent,#cover').on('click', function(){
 	$('#content').html("Loading")
 	$('#frame').removeClass("active");
 	$("#cover").removeClass("active");
 });
 /*Debugging Scripts*/
-$('#background-sw').on('click', function(){
+/*$('#background-sw').on('click', function(){
 	if($('body').css("background-image") == "none") {
 		$('body').css("background-image", "url('img/background-01.png')");
 		$('header').css("color", "#fff");
 		$('#animation-sw').addClass("active");
 		/*$("#animation-sw").animate({padding: "+20px"},{queue:true, duration:500, easing: 'swing'});
 		$("#animation-sw").animate({padding: "-20px"},{queue:true, duration:200, easing: 'swing'});*/
-	} else {
+	/*} else {
 		$('body').css("background-image", "none");
 		$('header').css("color", "#000");
 		$('#animation-sw').removeClass("active");
@@ -72,7 +76,8 @@ $('#animation-sw').on('click', function(){
 	} else {
 		$('body').css("animation-name", "none");
 	}
-});
+});*/
+// Map expand test
 $('#mapexpand-sw').on('click', function(){			
 	$("#earth").toggleClass("map");
 	$(".button").toggleClass("map");
